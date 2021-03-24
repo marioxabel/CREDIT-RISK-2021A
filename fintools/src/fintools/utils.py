53b507b5424b537ff26a7be0a1ab24211db4b2cb
rhdzmota <rhdzmota@gmail.com>
2021-03-23 19:25:58 -0600

    HW-DEF-FIBO Update fintools lib

---
+++ fintools/src/fintools/utils.py
@@ -0,0 +1,22 @@
+import functools
+import time
+from typing import Generic, TypeVar, Callable
+
+A = TypeVar('A')
+B = TypeVar('B')
+
+
+def compose(this: Callable[..., A], and_then: Callable[[A], B]) -> Callable[..., B]:
+    return lambda *x: and_then(this(*x))
+
+
+def timeit(logger):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            start = time.time()
+            output = func(*args, **kwargs)
+            logger.warn("Execution time %s" % (time.time() - start))
+            return output
+        return wrapper
+    return decorator
