4fc051653c32187651271f086e4a642be4c77cd7
Mario García <if711430@iteso.mx>
2021-03-29 21:33:25 -0600

    homework

--- src/homework/fibonacci/main.py
+++ src/homework/fibonacci/main.py
@@ -11,9 +11,27 @@ class Main:
     def __init__(self):
         logger.info("Main object initialized.")
 
+    @method_caching
     def element(self, position: int) -> int:
-        pass
+        n1, n2 = 0, 1
+        count = 0
 
-    @timeit(logger=logger)
-    def sequence(self, length: int) -> List[int]:
+        # check if the number of terms is valid
+        if position <= 0:
+            print("Please enter a positive integer")
+        elif position == 1:
+            print("Fibonacci sequence upto", position, ":")
+            print(n1)
+        else:
+            print("Fibonacci sequence:")
+            while count < position:
+                nth = n1 + n2
+                # update values
+                n1 = n2
+                n2 = nth
+                count += 1
+        print(n1)
+
+    @timeit
+    def sequence(self, length: int) -> int:
         pass
