34453188783a8cdb3458a9fec6d8d63f390469ea
Mario García <if711430@iteso.mx>
2021-04-14 14:32:31 -0600

    exam final version

--- src/exams/exam01/main.py
+++ src/exams/exam01/main.py
@@ -1,4 +1,3 @@
-import fire
 from .utils import flat_dictionary, display, get_dictionary
 
 
@@ -15,7 +14,3 @@ class Main:
     @staticmethod
     def flatten(filename: str):
         return display(flat_dictionary(get_dictionary(filename)))
-
-
-#if __name__ == "__main__":
- #   fire.Fire(Main)
